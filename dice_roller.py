32fb878174101b0c00b320e24aabba068d9c37c4
LenaPuh <LenaPuh95@gmail.com>
2020-07-01 20:32:24 +0300

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -3,7 +3,9 @@ import random
 
 def main():
 
-    dice_rolls = int(input('How many dice would you like to roll? '))
+    dice_rolls = 5
+    while dice_rolls >= 3:
+      dice_rolls = int(input('How many dice would you like to roll? '))
     dice_size = int(input('How many sides are the dice? '))
     dice_sum = 0
     for i in range(0, dice_rolls):
